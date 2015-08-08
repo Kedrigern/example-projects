a0d6e6eadb0da8fcad08b7d11fdae3c8f8cec5a6
Ondřej Profant <ondrej.profant@gmail.com>
2015-08-08 12:04:09 +0200

    Update md2pdf.sh
    Hidden temporaly files

--- bash/md2pdf.sh
+++ bash/md2pdf.sh
@@ -8,63 +8,59 @@
 # Global vars
 bin=pandoc
 output=""
-tmpdir="textmpdir"
+tmpdir=".md2pdf-textmpdir"
+errorFile=".md2pdf-errors.txt"
 
 function end() {
         rm -rf "$tmpdir"
         zenity --text "$1" --error;
-        exit 1
+        exit 1 
 }
 
 function input() {
-	if [ -z "$NAUTILUS_SCRIPT_SELECTED_FILE_PATHS" ]; then
-		echo "Nautilus doesn't not pass any data, use readme instead"
-		NAUTILUS_SCRIPT_SELECTED_FILE_PATHS=("readme.md" "readme2.md" "readme3.md")
-		cp "readme.md" "readme2.md"
-		cp "readme.md" "readme3.md"
-	fi;
-	file1=${NAUTILUS_SCRIPT_SELECTED_FILE_PATHS[0]}
-	file=$(basename "$file1")
-	output="${file%.*}.pdf"
+        if [ -z "$NAUTILUS_SCRIPT_SELECTED_FILE_PATHS" ]; then
+                echo "Nautilus does not pass any data, use readme instead"
+                NAUTILUS_SCRIPT_SELECTED_FILE_PATHS=("readme.md" "readme 2.md")
+                cp "readme.md" "readme 2.md"
+        fi;
+        file1="${NAUTILUS_SCRIPT_SELECTED_FILE_PATHS[0]}"
+        file=$(basename "$file1")
+        output="${file%.*}.pdf"
 }
 
 function convert() {
 
-	if $bin -v > /dev/null; then
-		mkdir -p "$tmpdir"	
-		errorFile=errors.txt
-		
+        if $bin -v > /dev/null; then
+                mkdir -p "$tmpdir"      
+                
                 # Todo: multiple files input
                 files=$(basename "${NAUTILUS_SCRIPT_SELECTED_FILE_PATHS[0]}")
-		
-		$bin -f markdown -t latex --latex-engine=xelatex \
-			--smart --normalize -s \
-			-o - "$files" \
-			2> "$errorFile" | \
-		vlna -f -v KkSsVvZzOoUuAaI 2>> "$errorFile" | \
-		xelatex -output-directory="$tmpdir" >> "$errorFile" 2>&1; \
-		rets=(${PIPESTATUS[*]})
+                $bin -f markdown -t latex --latex-engine=xelatex \
+                        --smart --normalize -s \
+                        -o - "$files" \
+                        2> "$errorFile" | \
+                vlna -f -v KkSsVvZzOoUuAaI 2>> "$errorFile" | \
+                xelatex -output-directory="$tmpdir" >> "$errorFile" 2>&1; \
+                rets=(${PIPESTATUS[*]})
 
-		if [ ${rets[0]} -ne 0 ]; then
-                        end "Pandoc vrátil kód ${rets[0]} pro $files ($output), podrobnosti v $errorFile"       
+                if [ ${rets[0]} -ne 0 ]; then
+                        end "Pandoc returns code ${rets[0]} for: $files ($output), more info in file: $errorFile"
                 fi;
                 if [ ${rets[1]} -ne 0 ]; then
-                        end "vlna vrátil kód ${rets[1]} pro $files ($output), podrobnosti v $errorFile"
+                        end "vlna returns code ${rets[1]} for: $files ($output), more info in file: $errorFile"
                 fi;
                 if [ ${rets[2]} -ne 0 ]; then
-                        end "xelatex vrátil kód ${rets[2]} pro $files ($output), podrobnosti v $errorFile"
+                        end "xelatex returns code ${rets[2]} for: $files ($output), more info in file: $errorFile"
                 fi;
-		
-		mv "$tmpdir/texput.pdf" "$output"
-		rm "$errorFile"
-		rm -rf "$tmpdir"
-	
-	else
-		zenity --text "Pandoc nenalezen" --error;
-		exit 1
-	fi;
+                
+                mv "$tmpdir/texput.pdf" "$output"
+                rm "$errorFile"
+                rm -rf "$tmpdir"
+        
+        else
+                end "Pandoc not found, path: $bin"
+        fi;
 }
 
 input
 convert
-
