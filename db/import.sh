f2ea2e1379d9e1bf7c843f00eab4a35131ff6fc8
Ondřej Profant <ondrej.profant@gmail.com>
2016-03-14 00:25:42 +0100

    Better import script

--- db/import.sh
+++ db/import.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
+# Author: Ondřej Profant
+# Imports CSV into mysql example
 
 file="data.csv"
 user=""
 db=""
-table=""
+table="pokus"
 host="localhost"
+
+lines=$(wc -l $file)
 
 mysql -u $user -p -h $host $db << EOF
 DROP TABLE IF EXISTS $table;
@@ -14,10 +18,12 @@ CREATE TABLE $table (
   castka decimal(12,2) NOT NULL,
   textik varchar(120) NOT NULL,
   datum1 date NOT NULL,
-  datum2 varchar(20) NOT NULL
+  datum2 date NOT NULL
 );
 
 LOAD DATA LOCAL INFILE '$file' INTO TABLE $table COLUMNS TERMINATED BY ';' ENCLOSED BY '"' IGNORE 1 LINES 
 (cislo, castka, textik, datum1, @var_datum2)
 set datum2 = STR_TO_DATE(@var_datum2, '%d.%m.%y');
+
+SELECT count(*) as imported, '$lines' as file FROM $table;
 EOF
