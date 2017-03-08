553b467bb01f0bad54bd38bd7d3d3e7bc4d16416
OndřejProfant <keddie@localhost.localdomain>
2017-03-08 13:19:32 +0100

    Python and sqlite

---
+++ python/sqlite/sqlite.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+
+"""
+Python3 and SQLite examples:
+    - datatypes
+    - select and subselects
+    - parse csv
+    - insert many
+    - named placeholders
+Database is in memory.
+
+Data about czech municipalities are from MVČR.
+"""
+
+import csv
+import sqlite3
+
+def load(file_obce, file_orp, connection):
+    c = connection.cursor()
+    c.execute('''CREATE TABLE obce (
+        id int,
+        name varchar,
+        orp_id int,
+        okres_id int,
+        typ varchar(1),
+        csu_id int,
+        pcd_id int
+    ) ''')
+
+    with open(file_obce) as infile:
+        reader = csv.reader(infile)
+        next(reader, None)  # skip the headers
+        for row in reader:
+            val7 = int(row[7]) if row[7] else None
+            values = (int(row[0]), row[1].strip(), int(row[2]), int(row[4]), row[5], int(row[6]), val7)
+            c.execute('INSERT INTO obce VALUES (?, ?, ?, ?, ?, ?, ?)', values)
+
+    c.execute('''CREATE TABLE orp (
+        id int,
+        name varchar,
+        csu_id int,
+        typ varchar(1),
+        okres_id int
+    ) ''')
+
+    with open(file_orp) as infile:
+        reader = csv.reader(infile)
+        next(reader, None)  # skip the headers
+        for row in reader:
+            id =     int(row[0]) if row[0].strip() else None
+            csu_id = int(row[2]) if row[2].strip() else None
+            okres =  int(row[4]) if row[4].strip() else None
+            values = (id, row[1].strip(), csu_id, row[3].strip(), okres)
+
+            c.execute('INSERT INTO orp VALUES (?, ?, ?, ?, ?)', values)
+
+    c.execute('''CREATE TABLE people (
+        name varchar,
+        salary int,
+        born date
+    ) ''')
+    people = [
+        ('Ala',  20800, '1990-09-20'),
+        ('Alan', 21600, '1989-10-20'),
+        ('Anna', 22500, '1988-11-15'),
+        ('Lala', 23500, '1987-12-01'),
+        ('Leo',  22100, '1986-05-13')
+    ]
+    c.executemany('INSERT INTO people VALUES (?,?,?)', people)
+
+    more_people = [
+        {'name': 'Jana', 'salary': 22100, 'born': '1990-03-03'},
+        {'name': 'Dana', 'salary': 22100, 'born': '1990-03-03'}
+    ]
+    c.executemany('INSERT INTO people VALUES (:name, :salary, :born)', more_people)
+    connection.commit()
+
+def examples(connection):
+    c = connection.cursor()
+
+    count = c.execute('SELECT count(*) FROM obce').fetchone()
+    print("Počet obcí: %d\n" % count)
+
+    obce = c.execute("SELECT * FROM obce WHERE name LIKE 'vejvanov%'").fetchall()
+    print("Obce jejiž jméno začíná vejvanov\n", obce)
+
+    answer = c.execute("SELECT name FROM obce WHERE orp_id IN (SELECT id FROM orp WHERE name = 'Černošice' or name = 'Beroun' )")
+    obce = []
+    for a in answer.fetchall():
+        obce.append(a[0])
+    print("Obce jejiž ORP jsou Černošice nebo Beroun:\n", obce)
+
+    answer = c.execute("SELECT * FROM people WHERE name LIKE '_ana'").fetchall()
+    print("Jména _ana:\n", answer)
+
+    answer = c.execute("SELECT * FROM people WHERE born BETWEEN '1987-01-01' AND '1989-01-01'").fetchall()
+    print("Narozeni 87-88:\n", answer)
+
+    connection.commit()
+
+def main():
+    conn = sqlite3.connect(':memory:')
+    load('obce.csv', 'orp.csv', conn)
+    examples(conn)
+    conn.close()
+
+if __name__ == '__main__':
+    main()
